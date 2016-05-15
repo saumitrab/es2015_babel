f9aa5c8fd0c947f920306cff9d819259404a7e26
Saumitra Bhanage <sbhanage@groupon.com>
2016-05-14 17:10:12 -0700

    Initial commit

---
+++ README.sh
@@ -0,0 +1,8 @@
+##Babel transpile example
+
+```
+npm install babel-cli -g
+npm install
+npm run transpile
+open index.html
+```
---
+++ transpile.sh
@@ -0,0 +1,2 @@
+#babel app.js.es6 --presets es2015 > app.js
+babel app.js.es6 > app.js
